cf16aea132be6b6bba761f026e02bcec937e563f
Nicholas Liang <ssss021418@gmail.com>
2018-05-02 06:06:46 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-
+"   |   |   ")
+      expect(rows[1]).to eq("-----------")
+      expect(rows[2]).to eq("   |   |   ")
+      expect(rows[3]).to eq("-----------")
+      expect(rows[4]).to eq("   |   |   "
 end
